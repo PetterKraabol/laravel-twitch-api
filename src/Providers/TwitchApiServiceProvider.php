9342e347151067b1a75a06a40bd52901ad293cee
Petter Kraabøl <petter.zarlach@gmail.com>
2015-11-06 21:15:12 +0100

    tidying up some code

--- src/Providers/TwitchApiServiceProvider.php
+++ src/Providers/TwitchApiServiceProvider.php
@@ -9,23 +9,11 @@ class TwitchApiServiceProvider extends ServiceProvider
     // Register service provider
     public function register()
     {
-        $this->registerServices();
+        $this->app->bind('Zarlach\TwitchApi\Services\TwitchApiService', 'Zarlach\TwitchApi\Services\TwitchApiService');
     }
 
     // Boot
     public function boot()
-    {
-        $this->addConfig();
-    }
-
-    // Register services
-    private function registerServices()
-    {
-        $this->app->bind('Zarlach\TwitchApi\Services\TwitchApiService', 'Zarlach\TwitchApi\Services\TwitchApiService');
-    }
-
-    // Add config file
-    private function addConfig()
     {
         $this->publishes([
             __DIR__.'/../../config/twitch-api.php' => config_path('twitch-api.php');
