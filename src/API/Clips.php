e2fff8463910fba14fbd36c9d4ff0da04e48130f
Vincen <vincen.prs@gmail.com>
2017-08-11 18:21:48 +0200

    add clips support

--- src/API/Clips.php
+++ src/API/Clips.php
@@ -3,7 +3,7 @@
 namespace Zarlach\TwitchApi\API;
 
 /**
- * Twitch documentation: https://github.com/justintv/Twitch-API/blob/master/v3_resources/xxx.md.
+ * Twitch documentation: https://github.com/justintv/Twitch-API/blob/master/v3_resources/clips.md.
  */
 class Clips extends Api
 {
